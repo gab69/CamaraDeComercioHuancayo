0c2f6d3fcac4a94cc8eabb21a87a8ac47389a326
gab69 <gabrielrc6979@gmail.com>
2024-03-04 17:18:32 -0500

    ruta corregida y seccion registro terminado

--- js/app.js
+++ js/app.js
@@ -26,7 +26,15 @@ document.querySelectorAll('button').forEach(button => {
   });
 });
 
+// FORMULARIO REGISTRAR E INICIAR SESION
+const container = document.querySelector(".container");
+const btnSignIn = document.getElementById("btn-sign-in");
+const btnSignUp = document.getElementById("btn-sign-up");
 
-
-
+btnSignIn.addEventListener("click",()=>{
+   container.classList.remove("toggle");
+});
+btnSignUp.addEventListener("click",()=>{
+   container.classList.add("toggle");
+});
 
